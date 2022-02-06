283169aae99f5afffcd63875526dcd81349faad2
gabriel <gabrielrsp18@gmail.com>
2022-02-06 17:43:43 -0300

    testes with useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useMemo, useState } from 'react';
+import { Item } from './components/Item';
 
-function App() {
+function App () {
+
+  const [items, setItems] = useState<string[]>([])
+  const [newItem, setNewItem] = useState('')
+  const [wishtList, setWishList] = useState<string[]>([])
+
+  function addItemToList () {
+    setItems([...items, `Item ${items.length}`])
+  }
+
+  //////////////////////////////////////////
+
+  //SEM USE MEMO
+  // const countItemsWithOne = () => (
+  //   [
+  //     console.log('test'),
+  //     items.filter(item => item.includes('1')).length
+  //   ]
+  // )
+
+  const countItemsWithOne = useMemo(() => (
+    [
+      console.log('test'),
+      items.filter(item => item.includes('1')).length
+    ]
+  ), [items])  // array de dependencias para comparação
+
+  //////////////////////////////////////////
+
+  /*
+    essa função será re-executada (sendo re-criado em memória) 
+    se houver alguma alteração nesse componente
+    ou seja, tudo que ela engloba entrará no fluxo de renderização,
+    aqui nesse caso há renderizaçã por causa do onChange do Input
+  */
+
+  // function addItemToWishList (item: string) {
+  //   setWishList([...wishtList, item])
+  // }
+
+
+  const addItemToWishList = useCallback((item: string) => {
+    setWishList(state => [...state, item])
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" onChange={e => setNewItem(e.target.value)} value={newItem} />
+      <button onClick={() => addItemToList()}>Add</button>
+      <ul>
+        {
+          items.map(item => {
+            return <Item key={item} onAddToWishList={addItemToWishList} title={item} />
+          })
+        }
+      </ul>
+
+      {/* <div>
+        SEM USE MEMO
+        Contagem: {countItemsWithOne()}
+      </div> */}
+
+      <div>
+        COM USE MEMO - Só executará o console.log se alterar o 'items'
+        O useMemo é para evitar RE-cálculos complexos
+        Contagem: {countItemsWithOne}
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Item.tsx
@@ -0,0 +1,18 @@
+import { memo } from 'react'
+
+type ItemProps = {
+  title: string;
+  onAddToWishList: (item: string) => void
+}
+
+function ItemComponent (props: ItemProps) {
+  return (
+    <li>{props.title}
+      <button onClick={() => props.onAddToWishList(props.title)}>
+        Add to Wishlist
+      </button>
+    </li>
+  )
+}
+
+export const Item = memo(ItemComponent) 
